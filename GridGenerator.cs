5f976c0c22d605388dbfd7dd86f810f32b526ca3
BenMiledGhada <52124991+BenMiledGhada@users.noreply.github.com>
2019-06-25 13:59:58 +0900

    Add files via upload

---
+++ GridGenerator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace _4Dtest_1
+{
+    public class GridGenerator
+    {
+        private const double dpiUnit = 25.4;
+
+        private int width;
+
+        private int height;
+
+        private int pitchLength;
+
+        private int dpi;
+
+        public GridGenerator(int Width, int Heigth, int PitchLengthInMm, int Dpi = 96)
+        {
+            SetGridSettings(Width, Heigth, PitchLengthInMm, Dpi);
+        }
+
+        /// <summary>
+        /// a function to set the parameters of the class 
+        /// </summary>
+        /// <param name="Width">Width of the image of the gratting</param>
+        /// <param name="Heigth">Heigth of the image of the gratting </param>
+        /// <param name="PitchLengthInMm">the length of pitch that user want in Mm</param>
+        /// <param name="Dpi">Dpi of the Monitor used; if not set it is automatically set to 96 </param>
+        public void SetGridSettings(int Width, int Heigth, int PitchLengthInMm, int Dpi = 96)
+        {
+            dpi = Dpi;
+            width = Width;
+            height = Heigth;
+            pitchLength = (int)(Dpi * (PitchLengthInMm / dpiUnit));
+        }
+
+        /// <summary>
+        /// Function for generating the pattern's array of the question 1-1
+        /// </summary>
+        /// <returns></returns>
+        public byte[] GenerateFirstGrid()
+        {
+            byte[] grid = new byte[height * width];
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    int position = (width * j) + i;
+                    int counter = (i + 1) / (pitchLength / 2);
+
+                    if ((counter % 2) == 0)
+                    { grid[position] = 0; }
+                    else
+                    { grid[position] = 255; }
+                }
+            }
+            return grid;
+        }
+
+        /// <summary>
+        /// Function for generating the array pattern's array of the question 1-2
+        /// </summary>
+        /// <returns></returns>
+        public byte[] GenerateSecondGrid()
+        {
+            int size = height * width;
+            byte[] grid = new byte[size];
+            double frequency = Math.PI / ((height * height) + (width * width));
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    int position = (width * j) + i;
+                    int horizontalCounter = (i + 1) / pitchLength;
+                    int verticalCounter = (j + 1) / pitchLength;
+
+                    if ((horizontalCounter % 2) == 0 || (verticalCounter % 2) == 0)
+                    { grid[position] = (byte)(255 - 255 * (Math.Cos(position * frequency))); }
+                    else
+                    { grid[position] = (byte)(255 * Math.Cos(position * frequency)); }
+                }
+            }
+            return grid;
+        }
+
+        /// <summary>
+        /// Function for converting the patterns from byte arrays into images
+        /// </summary>
+        /// <returns></returns>
+        public BitmapSource[] GenerateImages()
+        {
+            byte[] firstGrid = GenerateFirstGrid();
+            byte[] secondGrid = GenerateSecondGrid();
+
+            BitmapSource firstImage = BitmapSource.Create(width, height, dpi, dpi, System.Windows.Media.PixelFormats.Gray8, null, firstGrid, width);
+            BitmapSource secondImage = BitmapSource.Create(width, height, dpi, dpi, System.Windows.Media.PixelFormats.Gray8, null, secondGrid, width);
+
+            BitmapSource[] images = { firstImage, secondImage };
+
+            return images;
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace _4Dtest_1
+{
+    public partial class MainWindow : Window
+    {
+        public GridGenerator gridGenerator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initialization();
+        }
+        /// <summary>
+        /// TextBox and windows settings initialization, and creating the class for the grid generator
+        /// </summary>
+        private void Initialization()
+        {
+            WindowState = WindowState.Maximized;
+            gridGenerator = new GridGenerator(640, 480, 2);
+
+            heightBox.Text = "480";
+            widthBox.Text = "640";
+            pitchBox.Text = "2";
+            dpiBox.Text = "96";
+        }
+        /// <summary>
+        /// function that gets the settings from the settings boxes and passes them to the grid generatorclass
+        /// it catch an error if there  is one in the settings and revert to the initialization state
+        /// </summary>
+        private void ImageSettings()
+        {
+            try
+            {
+                int width = Int32.Parse(widthBox.Text);
+                int height = Int32.Parse(heightBox.Text);
+                int dpi = Int32.Parse(dpiBox.Text);
+                int pitch = Int32.Parse(pitchBox.Text);
+                gridGenerator.SetGridSettings(width, height, pitch, dpi);
+            }
+            catch
+            {
+                MessageBox.Show("Error in settings, Reverting to default settings", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                heightBox.Text = "480";
+                widthBox.Text = "640";
+                pitchBox.Text = "2";
+                dpiBox.Text = "96";
+                gridGenerator.SetGridSettings(640, 480, 2);
+            }
+        }
+        /// <summary>
+        /// Generate Button Event Click 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ImageSettings();
+            BitmapSource[] images = gridGenerator.GenerateImages();
+            firstImage.Source = images[0];
+            secondImage.Source = images[1];
+        }
+    }
+}
